537d31527ef5543d730e22c77e12a8a894ecf254
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-02-14 19:30:19 +0400

    there

--- sum.js
+++ sum.js
@@ -1,6 +1,9 @@
+
+// sul iyopa 3 mtavar punqciad  (setItems getItem delItems)
+
 const tasks = [];
 
-function setItems() {
+function setItems() { // local storage shi shenaxvis punqcia
   const button = document.querySelector("#button");
   const input = document.querySelector("#input");
 
@@ -10,35 +13,35 @@ function setItems() {
     } else {
       const task = input.value.trim();
       tasks.push(task);
-      const result = JSON.stringify(tasks);
+      const result = JSON.stringify(tasks); // gadadis JSON enaze (stirngebi roa ra) da ise vinaxavt
       localStorage.setItem("tasks", result);
-      input.value = "";
-      getItem();
+      input.value = ""; 
+      getItem();    //aq tu ar gamovidzaxebt gveqneba error 
     }
   });
 }
 
-function getItem() {
+function getItem() { // wamogebis da gamochenis punqcia
   const ul = document.querySelector("ul");
-  const data = JSON.parse(localStorage.getItem("tasks"));
-  ul.innerHTML = "";
+  const data = JSON.parse(localStorage.getItem("tasks")); // mogvaq arrey magram radgan JSON is enazea unda gaparse o
+  ul.innerHTML = ""; // amas tu ar dawer yovel axalis damatebaze winebs gaameorebs magalitad tu ukve gaq home da mere chawere table gamoitans 2 jer home da table
   data.forEach((datas, index) => {
     const li = document.createElement("li");
-    li.innerHTML = `${datas} <button id="delBtn" data-index="${index}">delete</button>`;
+    li.innerHTML = `${datas} <button id="delBtn" data-index="${index}">delete</button>`; // buttons vadzlevt atribut data-index es gexmareba rom washlis dros amoicnos romeli indexzea li da washalos
     ul.appendChild(li);
   });
-  delItems();
+  delItems();   // aq tu ar gamovidzaxebt gveqneba error radgan jer sheqmnili argvaq "li" tag
 }
 
-function delItems() {
-  const delbuttons = document.querySelectorAll("#delBtn");
+function delItems() { // washlis punqcia
+  const delbuttons = document.querySelectorAll("#delBtn"); // delbuttons aris Arrey radgan yvela axal sheqmnaze igive id aqvt
   delbuttons.forEach((item) => {
     item.addEventListener("click", function () {
-      const index = this.getAttribute("data-index");
-      tasks.splice(index, 1);
-      const result = JSON.stringify(tasks);
-      localStorage.setItem("tasks", result);
-      getItem();
+      const index = this.getAttribute("data-index"); // am midgomit vigebt ukve im 'li' tagis index
+      tasks.splice(index, 1); // es mtavari arrey dan shlis chvens 'li tags'
+      const result = JSON.stringify(tasks); // vanaxlebt Arrey is
+      localStorage.setItem("tasks", result); // isev Local storage shi vinaxavt
+      getItem(); // amas tu ar dawer arrey dan ki amoishleba elementi magram browsershi mainc geqneba tu ar daarefreshe
     });
   });
 }
